38939fb245db4adc4e757c943788d2d836bc0efc
mrrobot <planetearp@gmail.com>
2019-03-15 01:22:26 +0000

    Second commit with first Javascript

---
+++ index.js
@@ -0,0 +1,34 @@
+//initials bride groom date local
+
+const menu = document.getElementsByClassName('nav-item');
+const areaBlock = document.getElementsByClassName('area-block')
+
+for (let i = 0; i < menu.length; i++) {
+  const show = menu[i].attributes['show'].value;
+  menu[i].addEventListener('click', function() {
+    toggleVisibility(show);
+  })
+}
+
+
+// Makes each block visible
+
+function clearAll() {
+
+  for (i of areaBlock) {
+      i.style.visibility = "hidden";
+      }
+
+}
+
+function toggleVisibility(show) {
+    
+    clearAll();
+    document.getElementById(show).style.visibility = "visible";
+
+  }
+
+clearAll();
+// function changeClass(newClass) {
+//   document.getElementById("inv").className = newClass;
+// }
